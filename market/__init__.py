b71cd65cb9b2d3ee2ab5a42a30a43b011da663be
Binit <binitlekhak@gmail.com>
2022-06-18 16:30:24 +0545

    adding the project to the repositry

---
+++ market/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '58560ed4fdc1766af8d803623a'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+from market import routes
